003c9de427c8b3979db8457dd1d52ed153bd0d82
evan999 <emeshberge@gmail.com>
2018-11-08 20:58:26 -0500

    Create login route and token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://evan:toonfan1@ds151753.mlab.com:51753/devconnector_mern"
+  mongoURI: "mongodb://evan:toonfan1@ds151753.mlab.com:51753/devconnector_mern",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -21,13 +22,17 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+// Connect to MongoDB
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
