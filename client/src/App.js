ba78f4732bc38b53c62d1255700ef394a3e6dfc8
evan999 <emeshberge@gmail.com>
2018-11-17 00:33:58 -0500

    Create and intialize layout components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-        <header className="App-header">
-          <h1>My React App</h1>
-        </header>
-=======
-        <h1>My React App</h1>
->>>>>>> bootstrap-assets-setup
+          <Navbar />
+          <Landing />
+          <Footer />
       </div>
     );
   }
