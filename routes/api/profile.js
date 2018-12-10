272a077af6765878c1293b3c3d6d491e209a8390
evan999 <emeshberge@gmail.com>
2018-12-09 22:14:05 -0500

    Clean up styling a little

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -276,7 +276,6 @@ router.delete(
 // @route  DELETE api/profile
 // @desc   Delete user and profile
 // @access Private
-
 router.delete(
 	'/', 
 	passport.authenticate('jwt', { session: false }), 
