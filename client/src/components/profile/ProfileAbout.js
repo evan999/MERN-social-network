201d6485c650a4dad9ac4e2218df7bbbafeebc23
evan999 <emeshberge@gmail.com>
2019-01-07 00:55:53 -0500

    Profile Header page

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 
 class ProfileAbout extends Component {
+	
 	render () {
 		return (
 			<div>
 				<h1>TODO: PROFILE ABOUT</h1>
 			</div>
-		)
+		);
 	}
 
 }
