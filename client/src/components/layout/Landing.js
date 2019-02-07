2c05eb4ec73ae117dcefbf05be028e9698825986
evan999 <emeshberge@gmail.com>
2019-02-06 22:31:32 -0500

    Change site name

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -18,9 +18,9 @@ class Landing extends Component {
 				<div className="container">
 					<div className="row">
 						<div className="col-md-12 text-center">
-							<h1 className="display-3 mb-4">Developer Connector
+							<h1 className="display-3 mb-4">DevNet
 							</h1>
-							<p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+							<p className="lead"> Connect with developers all over the world. Share stories, tips and advice and get help from other developers.</p>
 							<hr />
 							<Link to="/register" className="btn btn-lg btn-info mr-2">
 								Sign Up
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -58,27 +58,27 @@ class Navbar extends Component {
     );
 
 		return (
-  <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-    <div className="container">
-      <Link className="navbar-brand" to="/">
-			DevConnector
-			</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+          DevNet
+          </Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-      <div className="collapse navbar-collapse" id="mobile-nav">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-						<Link className="nav-link" to="/profiles"> 
-						{' '} Developers
-            </Link>
-          </li>
-        </ul>
-        {isAuthenticated ? authLinks : guestLinks} 
-      </div>
-    </div>
-  </nav>			
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/profiles"> 
+                {' '} Developers
+                </Link>
+              </li>
+            </ul>
+            {isAuthenticated ? authLinks : guestLinks} 
+          </div>
+        </div>
+      </nav>			
 		)
 	}
 }
